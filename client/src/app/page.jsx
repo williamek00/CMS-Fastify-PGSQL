b1e9a45ae211cdf813b52f169119983e732eae69
williamek00 <williamenosk123@gmail.com>
2023-09-13 06:06:10 +0700

    Fixing table bugs

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 import "./globals.css";
 import React, { useState, useEffect } from "react";
-import Sidebar from '../components/sidebar'
-
+import Sidebar from "../components/sidebar";
 export default function Admin() {
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -14,12 +14,32 @@ export default function Admin() {
     return () => clearInterval(intervalId);
   }, []);
 
+  const toggleSidebar = () => {
+    setIsActive(!isActive);
+  };
 
+  const serverRenderedDate = currentDate.toLocaleString("en-US", {
+    hour12: true, // Use 12-hour format with AM/PM
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
 
   return (
     <div className="container">
-      <Sidebar />
+      <Sidebar isActive={isActive} isHidden={"is-hidden"} />
       <div className="contentContainer">
+        <div className="button-float">
+          <img
+            src="https://cdn.icon-icons.com/icons2/2761/PNG/512/menu_hamburger_burger_icon_176431.png"
+            alt=""
+            style={{ width: "35px", height: "35px" }}
+            onClick={toggleSidebar}
+          />
+        </div>
         <div className="date-container">
           <p className="date-text user-login">User 1</p>
           <p className="date-text">{currentDate.toLocaleString()}</p>
@@ -29,46 +49,50 @@ export default function Admin() {
         </div>
         <div className="tableContainer">
           <table className="table">
-            <tr>
-              <th>No</th>
-              <th>Produk</th>
-              <th>Nama Pemborong</th>
-              <th>Nama Vendor</th>
-              <th>dummy</th>
-              <th>test</th>
-              <th>Nama</th>
-              <th>Nama</th>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-              <td>ANTO</td>
-            </tr>
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Produk</th>
+                <th>Nama Pemborong</th>
+                <th>Nama Vendor</th>
+                <th>dummy</th>
+                <th>test</th>
+                <th>Nama</th>
+                <th>Nama</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+                <td>ANTO</td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -1,21 +1,17 @@
-"use client"
+"use client";
 import "../app/globals.css";
 import React, { useState, useEffect } from "react";
 
-export default function Sidebar() {
-    const [isActive, setIsActive] = useState(false);
-    const toggleSidebar = () => {
-        setIsActive(!isActive);
-    };
-    return (
-        <nav className={`sidebar ${isActive ? "is-hidden" : ""}`}>
-            <ul>
-                <li>Home</li>
-                <li>Notification</li>
-                <li>Memo</li>
-                <li>Order</li>
-                <li>History</li>
-            </ul>
-        </nav>
-    )
+export default function Sidebar({ isActive, isHidden }) {
+  return (
+    <nav className={`sidebar ${isActive ? isHidden : ""}`}>
+      <ul>
+        <li>Home</li>
+        <li>Notification</li>
+        <li>Memo</li>
+        <li>Order</li>
+        <li>History</li>
+      </ul>
+    </nav>
+  );
 }
